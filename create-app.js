32880a768f3198e39470360e0f2feced3e5086f4
Alexander Lee <alex8usa8@gmail.com>
2017-11-12 16:34:48 -0800

    Adjusted behavior of multer on where the image file should be saved and adjust the naming convention of the image file. #2

--- create-app.js
+++ create-app.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const path = require('path')
-
+const multer = require('multer')
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/conference/images/uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + '.jpg')
+  }
+})
+const upload = multer({ storage: storage })
+console.log(upload)
 module.exports = function createApp() {
   const app = express()
   app.use(express.static(path.join(__dirname, 'public')))
