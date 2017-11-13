5acb8544e84ece25f1780ca8b0ac639574fcd406
Alexander Lee <alex8usa8@gmail.com>
2017-11-13 14:51:05 -0800

    In the registered POST route for /signin, code block is adjusted to get the data URI and upload it to cloudinary. Query string is sent the necessary username and user-image URL information for conference/index.js to extract. #2

--- create-app.js
+++ create-app.js
@@ -5,8 +5,8 @@ const querystring = require('querystring')
 const multer = require('multer')
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
-const datauri = require('datauri')
-const dUri = new datauri()
+const Datauri = require('datauri')
+const dUri = new Datauri()
 const cloudinary = require('cloudinary')
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -19,12 +19,15 @@ module.exports = function createApp() {
   app.use(express.static(path.join(__dirname, 'public')))
 
   app.post('/signin', upload.single('user-image'), (req, res) => {
-    const imageObject = {
-      'user-image': req.file.filename
-    }
-    const queryString = '/?' + querystring.stringify(req.body) + '&' +
-      querystring.stringify(imageObject)
-    res.redirect('/conference' + queryString)
+    const imageDataUri = dUri.format('.jpg', req.file.buffer).content
+    cloudinary.uploader.upload(imageDataUri, result => {
+      const queryObject = {
+        username: req.body.username,
+        'user-image': result.secure_url
+      }
+      const queryString = '/?' + querystring.stringify(queryObject)
+      res.redirect('/conference' + queryString)
+    })
   })
   return app
 }
