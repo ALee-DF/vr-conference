3ad556ff9f81c06e345c1cc62f254a1f4927a23b
Alexander Lee <alex8usa8@gmail.com>
2017-11-16 14:57:02 -0800

    Adjusted create-app.js in the root directory of the repository, public/conference/index.js, and public/conference/renderAvatar.js to assign a special uuid that will be used as an identifier for the newly created avatar. #3

--- public/conference/index.js
+++ public/conference/index.js
@@ -1,16 +1,20 @@
 /* global AFRAME */
 /* global renderAvatar */
+let avatarID
 AFRAME.registerComponent('spawn-point', {
   init: function () {
     const params = new URLSearchParams(window.location.search)
+    const username = params.get('username')
+    const userImage = params.get('user-image')
+    avatarID = params.get('id')
     const $spawnPoint = document.querySelector('#spawn-point')
-    $spawnPoint.appendChild(renderAvatar(params.get('username'), params.get('user-image')))
+    $spawnPoint.appendChild(renderAvatar(username, userImage, avatarID))
   }
 })
 
 AFRAME.registerComponent('avatar-position', {
   tick: function () {
-    const $avatar = document.querySelector('#avatar')
+    const $avatar = document.querySelector('#' + avatarID)
     const avatarPosition = this.el.getAttribute('position')
     const avatarRotation = this.el.getAttribute('rotation')
     $avatar.setAttribute('position', avatarPosition.x + ' ' + avatarPosition.y + ' ' + avatarPosition.z)
