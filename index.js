329e1e8e97bcf777becdc969f13b8b48b9769224
Alexander Lee <alex8usa8@gmail.com>
2017-11-16 15:54:42 -0800

    Recorded the user data into an avatarsList array and correlated the socket ID to the avatar ID in idCorrelate object. #3

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ server.listen(process.env.PORT, () => {
   console.log('Listening on Port', process.env.PORT)
 })
 
+const avatarsList = []
+const idCorrelate = {}
 io.on('connection', function (socket) {
   socket.on('user joined', function (userdata) {
+    avatarsList.push(userdata)
+    idCorrelate[socket.id] = userdata.avatarID
   })
 })
