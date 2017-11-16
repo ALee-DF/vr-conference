6869874fcaacce81ab6ecce2677aef3830944b73
Alexander Lee <alex8usa8@gmail.com>
2017-11-16 15:51:54 -0800

    Set up index.js in the root directory to receive information from the "user joined" event. #3

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ const io = require('socket.io')(server)
 server.listen(process.env.PORT, () => {
   console.log('Listening on Port', process.env.PORT)
 })
+
+io.on('connection', function (socket) {
+  socket.on('user joined', function (userdata) {
+  })
+})
