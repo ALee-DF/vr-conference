50021801d6d5d5064e3781b7eca6d0bd8cbd1772
Alexander Lee <alex8usa8@gmail.com>
2017-11-16 15:22:32 -0800

    In the index.js file located in the root directory of the repository, supply an HTTP server. #3

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 const createApp = require('./create-app')
 
 const app = createApp()
-app.listen(process.env.PORT, () => {
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+server.listen(process.env.PORT, () => {
   console.log('Listening on Port', process.env.PORT)
 })
