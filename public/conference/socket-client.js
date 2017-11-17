0552cfce4ac15df6a248e971a80ce7916e619398
Alexander Lee <alex8usa8@gmail.com>
2017-11-16 16:21:38 -0800

    In socket-client.js, handled the "update avatar position and rotation" event to update the avatars not belonging to the user. #3

--- public/conference/socket-client.js
+++ public/conference/socket-client.js
@@ -5,3 +5,11 @@ socket.on('new user', function ({ username, userImage, avatarID }) {
   const $spawnPoint = document.querySelector('#spawn-point')
   $spawnPoint.appendChild(renderAvatar(username, userImage, avatarID))
 })
+
+socket.on('update avatar position and rotation', function ({ avatarPosition, avatarRotation, avatarID }) {
+  const $avatar = document.querySelector('#' + avatarID)
+  if ($avatar) {
+    $avatar.setAttribute('position', avatarPosition.x + ' ' + avatarPosition.y + ' ' + avatarPosition.z)
+    $avatar.setAttribute('rotation', avatarRotation.x + ' ' + avatarRotation.y + ' ' + avatarRotation.z)
+  }
+})
