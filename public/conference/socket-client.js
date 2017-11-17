c20c0ba6c54428722a32f89e9f9bf1faf2a9e30a
Alexander Lee <alex8usa8@gmail.com>
2017-11-16 16:04:22 -0800

    In socket-client.js, we have a socket that receives the "new user" event and append the avatar to the spawn point. #3

--- public/conference/socket-client.js
+++ public/conference/socket-client.js
@@ -1,2 +1,7 @@
+/* global renderAvatar */
 /* global io */
 const socket = io.connect('http://localhost:3000')
+socket.on('new user', function ({ username, userImage, avatarID }) {
+  const $spawnPoint = document.querySelector('#spawn-point')
+  $spawnPoint.appendChild(renderAvatar(username, userImage, avatarID))
+})
